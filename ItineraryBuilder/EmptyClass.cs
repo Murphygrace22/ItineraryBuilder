f39bb0890fcd7c5b02c99991834927e4924af586
Murphygrace22 <grace.murphy22@yahoo.com>
2022-11-03 13:34:02 -0400

    Create a JSON file and README

--- ItineraryBuilder/EmptyClass.cs
+++ ItineraryBuilder/EmptyClass.cs
@@ -1,10 +1,26 @@
-﻿using System;
-namespace ItineraryBuilder
+﻿// Root myDeserializedClass = JsonConvert.DeserializeObject<List<Root>>(myJsonResponse);
+public class ActivitiesForFL
 {
-    public class EmptyClass
-    {
-        public EmptyClass()
-        {
-        }
-    }
+    public string Name { get; set; }
 }
+
+public class ActivitiesForKY
+{
+    public string Name { get; set; }
+}
+
+public class Root
+{
+    [JsonProperty("State Option 1")]
+    public string StateOption1 { get; set; }
+
+    [JsonProperty("Activities For KY")]
+    public List<ActivitiesForKY> ActivitiesForKY { get; set; }
+
+    [JsonProperty("State Option 2")]
+    public string StateOption2 { get; set; }
+
+    [JsonProperty("Activities for FL")]
+    public List<ActivitiesForFL> ActivitiesForFL { get; set; }
+}
+
---
+++ ItineraryBuilder/README.md
@@ -0,0 +1,8 @@
+This project builds an itinerary for different states that you choose from. 
+Each State has 3 indoor activity options and 3 outdoor activity options.
+You then choose the activities you'd like to keep. 
+
+Download the files from Github and run it on your machine, everything will be prompted. 
+
+-Created by Grace Murphy 
+November 2022
